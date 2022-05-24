7769800ebee8e24372edf97e18c87c70ae10423e
MatsurSviat <hereyourare@rambler.ru>
2022-05-24 21:35:54 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,43 +1,17 @@
 
 const fs = require('fs');
-const { dirname } = require('path');
 const path = require('path');
 
- fs.mkdir(path.join(__dirname, 'files-copy'), {
-   recursive: true
-}, function(err) {
-   if (err) {
-      console.log(err)
-   } else {
-      console.log("New directory successfully created.")
-   }
-})
+(async () => {
 
-// fs.readdir(path.join(__dirname, './files'), { withFileTypes: true }, (error, files) => {       
-    
-//     if (error) 
-//  console.log(error);
-// else {
-//     const filesArray = files.filter(fileName => fileName.isFile()).map(fileName => fileName.name);
-
-//     const filesLink = filesArray.map(e => './files/' + e);
-    
-//     const filesCopy = filesLink.map((a) => {
-       
-//           return fs.copyFile(path.join(__dirname, a), path.join(__dirname, './files-copy'), (error, files) => {
-//             if (error) 
-//             console.log(error) 
-//             else {
-//                 return 
-//             }
-//           });
-            
-//       });
-// }  
-
-// })
-
-// fs.copyFile (path.join(dirname, '../test-css.css'), path.join(dirname, './files-copy'), (err) => {
-//     if (err) throw err;
-//   console.log('File was copied to destination');
-// }) 
+  if ((await fs.promises.readdir(path.join(__dirname))).includes('files-copy')){
+    await fs.promises.rm(path.join(__dirname, "files-copy"), { recursive: true}); 
+  }
+  await fs.promises.mkdir(path.join(__dirname, "files-copy"), { recursive: false});
+  let filesFromOld = await fs.promises.readdir(path.join(__dirname, 'files'))
+  for (let file of filesFromOld){
+    await fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+  }
+  console.log('Done!');
+  
+})();
