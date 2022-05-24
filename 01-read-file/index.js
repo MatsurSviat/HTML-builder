de883c87cffe436742edec4ce48ea1d08c11f124
MatsurSviat <hereyourare@rambler.ru>
2022-05-24 20:58:19 +0300

    feat: change first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,9 @@
+
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(
-    path.join(__dirname, './text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    }
-);
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let fullContent = '';
+readStream.on('data', chunk => fullContent += chunk);
+readStream.on('end', () => process.stdout.write(fullContent));
